1fed7349fa87bd37c1b2df425b16d9c8e246187b
Claude Barde <25201109+claudebarde@users.noreply.github.com>
2020-08-15 14:54:31 +0300

    Added support for FA1.2 token faucet

--- lib/tokenFaucetInteractions.ts
+++ lib/tokenFaucetInteractions.ts
@@ -14,20 +14,55 @@ export default async ({
   network
 }) => {
   const { selectTokenFaucet } = await inquirer.selectTokenFaucet();
-  if (selectTokenFaucet === TokenFaucetType.FA12) {
+  /*if (selectTokenFaucet === TokenFaucetType.FA12) {
     // FA1.2
-  } else if (selectTokenFaucet === TokenFaucetType.FA2_FT) {
-    // FA2 Fungible tokens
+    const loadingBalance = ora({
+      text: "Searching for your FA1.2 fungible token balance",
+      color: "cyan"
+    }).start();
+    // creates instance of contract
+    const contract = await Tezos.contract.at(tokenFaucets.FA12);
+    // creates storage
+    const storage: any = await contract.storage();
+    // gets user's token balance
+    const balance = await storage.ledger.get(publicKeyHash);
+    if (balance) {
+      loadingBalance.info(
+        `Your token balance: ${chalk.bold(balance.toNumber() + " tokens")}`
+      );
+    } else {
+      loadingBalance.info("No balance");
+    }
+
+
+  } else*/
+
+  if (
+    selectTokenFaucet === TokenFaucetType.FA12 ||
+    selectTokenFaucet === TokenFaucetType.FA2_FT
+  ) {
+    // FA1.2 and FA2 Fungible tokens
     const loadingBalance = ora({
       text: "Searching for your token balance",
       color: "cyan"
     }).start();
     // creates instance of contract
-    const contract = await Tezos.contract.at(tokenFaucets.FA2_FT);
+    let contract;
+    if (selectTokenFaucet === TokenFaucetType.FA12) {
+      contract = await Tezos.contract.at(tokenFaucets.FA12);
+    } else if (selectTokenFaucet === TokenFaucetType.FA2_FT) {
+      contract = await Tezos.contract.at(tokenFaucets.FA2_FT);
+    }
     // creates storage
     const storage: any = await contract.storage();
     // gets user's token balance
-    const balance = await storage.assets.ledger.get(publicKeyHash);
+    let balance;
+    if (selectTokenFaucet === TokenFaucetType.FA12) {
+      const account = await storage.ledger.get(publicKeyHash);
+      balance = account.balance;
+    } else if (selectTokenFaucet === TokenFaucetType.FA2_FT) {
+      balance = await storage.assets.ledger.get(publicKeyHash);
+    }
     if (balance) {
       loadingBalance.info(
         `Your token balance: ${chalk.bold(balance.toNumber() + " tokens")}`
@@ -61,9 +96,14 @@ export default async ({
           } else {
             owner = inputAddress;
           }
-          const op = await contract.methods
-            .mint_tokens([{ owner, amount: inputAmount }])
-            .send();
+          let op;
+          if (selectTokenFaucet === TokenFaucetType.FA12) {
+            op = await contract.methods.mint(publicKeyHash, inputAmount).send();
+          } else if (selectTokenFaucet === TokenFaucetType.FA2_FT) {
+            op = await contract.methods
+              .mint_tokens([{ owner, amount: inputAmount }])
+              .send();
+          }
           // creates clickable link in terminal emulators
           const link = terminalLink(
             op.hash,
@@ -103,19 +143,26 @@ export default async ({
             color: "cyan"
           }).start();
           // prepare for transfer
-          let owner;
+          let to_;
           if (recipient === "own") {
-            owner = publicKeyHash;
+            to_ = publicKeyHash;
           } else {
-            owner = recipient;
+            to_ = recipient;
           }
-          const transfer = [
-            {
-              from_: publicKeyHash,
-              txs: [{ to_: recipient, token_id: 0, amount: inputAmount }]
-            }
-          ];
-          const op = await contract.methods.transfer(transfer).send();
+          let op;
+          if (selectTokenFaucet === TokenFaucetType.FA12) {
+            op = await contract.methods
+              .transfer(publicKeyHash, to_, inputAmount)
+              .send();
+          } else if (selectTokenFaucet === TokenFaucetType.FA2_FT) {
+            const transfer = [
+              {
+                from_: publicKeyHash,
+                txs: [{ to_, token_id: 0, amount: inputAmount }]
+              }
+            ];
+            op = await contract.methods.transfer(transfer).send();
+          }
           // creates clickable link in terminal emulators
           const link = terminalLink(
             op.hash,
