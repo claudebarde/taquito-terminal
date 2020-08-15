42f3f4ec6020accce6dc2193609ba025012ea28e
Claude Barde <25201109+claudebarde@users.noreply.github.com>
2020-08-15 17:58:57 +0300

    Added more info for contracts

--- lib/getContractInfo.ts
+++ lib/getContractInfo.ts
@@ -14,11 +14,21 @@ export default async ({ Tezos, publicKeyHash, signingMethod, network }) => {
   const contract = await Tezos.contract.at(inputContractAddress);
   // gets contract info
   const balance = await Tezos.tz.getBalance(inputContractAddress);
-  loadingContractInfo.succeed(
+  const storage = await contract.storage();
+  loadingContractInfo.info(
     chalk.white.bold(`Contract balance: XTZ ${balance}`)
   );
   console.log(chalk.blue("ℹ ") + chalk.white.bold(`Contract entrypoints:`));
   Object.keys(contract.methods).forEach(method =>
     console.log(chalk.white.bold(`    • ${method}`))
   );
+  console.log(chalk.blue("ℹ ") + chalk.white.bold(`Contract storage:`));
+  Object.keys(storage).forEach(el => {
+    console.log(chalk.white.bold(`    • ${el}`));
+    if (typeof storage[el] === "object") {
+      Object.keys(storage[el]).forEach(nestedEl => {
+        console.log(chalk.white(`      • ${nestedEl}`));
+      });
+    }
+  });
 };
